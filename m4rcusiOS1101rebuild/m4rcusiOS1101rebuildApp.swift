96dff40d117878057b64869cae2b0cb2ebf2ab16
m4rcus <dauntlessstyle31415926@gmail.com>
2021-10-15 11:27:51 +0800

    Initial Commit

---
+++ m4rcusiOS1101rebuild/m4rcusiOS1101rebuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  m4rcusiOS1101rebuildApp.swift
+//  m4rcusiOS1101rebuild
+//
+//  Created by User03 on 2021/10/15.
+//
+
+import SwiftUI
+
+@main
+struct m4rcusiOS1101rebuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
